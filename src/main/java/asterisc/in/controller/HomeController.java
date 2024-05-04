7187e9886f8d5304f2af44d49cd6f3c3f94e6a39
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-05-04 15:01:18 +0530

    Created controller class

---
+++ src/main/java/asterisc/in/controller/HomeController.java
@@ -0,0 +1,15 @@
+package asterisc.in.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping("/test")
+	String myfun()
+	{
+		return "index.jsp";
+	}
+}
